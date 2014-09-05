27bf4c4bf59be7f18f579affbfe292e00e067e2d
Rodolphe Edouard <rodoard@gmail.com>
2014-09-05 22:55:06 +0700

    remove 'websocket' constraint on endpoint
    can be arbitrary now including websocket

--- app/assets/javascripts/websockets.js.coffee
+++ app/assets/javascripts/websockets.js.coffee
@@ -1,9 +1,10 @@
 class WebSockets
+  scheme: "ws://"
   constructor: (websocket_server) ->
-    @dispatcher = new WebSocketRails(websocket_server)
+    @websocket = new WebSocket("#{@scheme}#{websocket_server}/");
     @
   subscribe: (chanel, event, action) ->
-    chanel = @dispatcher.subscribe(chanel)
-    chanel.bind event, action
+    @websocket.onmessage = (message) ->
+      action(message.data)
     @
 window.WebSockets = WebSockets
--- app/middleware/trap_requests.rb
+++ app/middleware/trap_requests.rb
@@ -1,22 +1,46 @@
 class TrapRequests
+  KEEPALIVE_TIME = 15 # in seconds
   def initialize(app)
     @app = app
+    @clients = []
   end
+
   def call(env)
-    status, headers, response = @app.call(env)
-    unless /^\/assets.*/ =~ env['REQUEST_PATH'] || /^\/websocket/ =~ env['REQUEST_PATH']
-      request = Rack::Request.new(env)
-      info = request.path_info.split('/')
-      info.shift
-      if info.length == 1 and info.first != 'new'
-        trap_id = request.env["action_dispatch.request.parameters"]['trap_id']
-        trap_request = Trap.add_new_request trap_id, request_info(request), response.body, request.request_method
-        template = File.read(File.join(Rails.root, "app", "views", "trap", "_request.html.erb"))
-        output = Erubis::Eruby.new(template).result(request:trap_request)
-        WebsocketRails[:requests].trigger :new, output
+    if Faye::WebSocket.websocket?(env)
+
+      # WebSockets logic goes here
+      ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+      ws.on :open do |event|
+        @clients << ws
       end
-     end
-    [status, headers, response]
+      ws.on :message do |event|
+        p [:message, event.data]
+        @clients.each {|client| client.send(event.data) }
+      end
+      ws.on :close do |event|
+        p [:close, ws.object_id, event.code, event.reason]
+        @clients.delete(ws)
+        ws = nil
+      end
+      # Return async Rack response
+      ws.rack_response
+    else
+      status, headers, response = @app.call(env)
+      unless /^\/assets.*/ =~ env['REQUEST_PATH']
+        request = Rack::Request.new(env)
+        info = request.path_info.split('/')
+        info.shift
+        if info.length == 1 and info.first != 'new'
+          trap_id = request.env["action_dispatch.request.parameters"]['trap_id']
+          trap_request = Trap.add_new_request trap_id, request_info(request), response.body, request.request_method
+          template = File.read(File.join(Rails.root, "app", "views", "trap", "_request.html.erb"))
+          output = Erubis::Eruby.new(template).result(request:trap_request)
+          Rails.logger.error @clients.count
+          @clients.each {|client| client.send(output) }
+        end
+      end
+      [status, headers, response]
+    end
   end
   private
   def request_info(request)
